6a8beebb40642571dca167adc0bbffb7b9062b66
valesquiveel <amandallmoreira@gmail.com>
2023-12-04 17:53:11 -0300

    v10
    Atividade aula 09

---
+++ aula09/exercicios/js/script01.js
@@ -0,0 +1,9 @@
+// 1) Fazer um script de tabuada sendo que o usuario possa escolher de 1 a 10
+// 2) Implementar o botão do tema dark e tema light
+
+// TABUADA
+
+let number = null;
+  for(let i = 1; i <= 10; i++) {
+      document.write(number + ' x ' + i + ' = ' + number * i + '<br>');
+  }
